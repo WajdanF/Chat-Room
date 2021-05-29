eb5690118fe53968eac1f18e0335eff6437d270c
Wajdan-Faheem <wajdanf02@gmail.com>
2021-05-29 03:56:21 -0400

    pushing

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,48 @@
 //dom queries
 const chatList = document.querySelector(".chat-list");
+const newChat = document.querySelector(".new-chat");
+const newName = document.querySelector(".new-name");
+const update = document.querySelector('.update-mssg');
 
-const chatroom = new Chatroom('general','zhenia');
-const chatUI = new ChatUI(chatList)
+const chatroom = new Chatroom("general", "zhenia");
+const chatUI = new ChatUI(chatList);
 //ggetting chat
 
-chatroom.getChat((data)=>{
-    chatUI.render(data)
-})
+chatroom.getChat((data) => {
+    chatUI.render(data);
+});
+
+//adding new chat
+newChat.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (localStorage.getItem('username') !== null){
+    const message = newChat.message.value.trim();
+    chatroom
+        .addChat(message)
+        .then(() => {
+            newChat.reset();
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+    }
+    else{
+        alert('Please setup a username!')
+        newChat.reset();
+    }
+    });
+
+newName.addEventListener("submit", (e) => {
+    e.preventDefault();
+    nameValue = newName.name.value.trim();
+    localStorage.setItem("username", nameValue);
+    chatroom.updateName(nameValue);
+    newName.classList.add("d-none");
+    console.log(update.textContent,'lol');
+    update.innerHTML = `<span style="font-weight:bold">Current Username:</span> <span> ${localStorage.username}</span>`;
+});
+
+if (localStorage.getItem("username") !== null) {
+    newName.classList.add("d-none");
+    update.innerHTML = `<span style="font-weight:bold">Current Username:</span> <span> ${localStorage.username}</span>`;
+}
